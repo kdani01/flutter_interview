048fd2a50a0d649128187bd4bec7dfbab706d74d
Daniel Kara <kara.dani17@gmail.com>
2024-03-21 22:54:35 +0100

    feat gridview and fetch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -26,10 +28,7 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          children: [],
-        ),
+        body: const ProductGridView(),
         bottomNavigationBar: const BottomMenu(),
       ),
     );
@@ -105,6 +104,145 @@ class CustomMenuButton extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class ProductGridView extends StatefulWidget {
+  const ProductGridView({super.key});
+
+  @override
+  ProductGridViewState createState() => ProductGridViewState();
+}
+
+class ProductGridViewState extends State<ProductGridView> {
+  late ScrollController scrollController;
+  List<dynamic> productList = [];
+  int numberOfDisplayedProducts = 0;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = ScrollController()..addListener(scrollListener);
+    fetchNextProducts();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  void scrollListener() {
+    if (!isLoading &&
+        scrollController.position.pixels ==
+            scrollController.position.maxScrollExtent) {
+      fetchNextProducts();
+    }
+  }
+
+  Future<void> fetchNextProducts() async {
+    if (!isLoading) {
+      setState(() {
+        isLoading = true;
+      });
+
+      final response = await http.get(Uri.parse(
+          'https://dummyjson.com/products?limit=8&skip=$numberOfDisplayedProducts&'));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> productsLoading =
+            json.decode(response.body)['products'];
+        setState(() {
+          productList.addAll(productsLoading);
+          numberOfDisplayedProducts += 8;
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load products');
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      controller: scrollController,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.6,
+      ),
+      itemCount: productList.length + (isLoading ? 1 : 0),
+      itemBuilder: (context, index) {
+        if (index < productList.length) {
+          final product = productList[index];
+          return GridTile(
+            child: Card(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              semanticContainer: true,
+              shape: BeveledRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+              elevation: 0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  FractionallySizedBox(
+                    widthFactor: 1,
+                    child: ClipRect(
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        heightFactor: 0.5,
+                        child: SizedBox(
+                          height: 360,
+                          child: Image.network(
+                            product['thumbnail'],
+                            fit: BoxFit.cover,
+                            loadingBuilder: (context, child, loadingProgress) {
+                              if (loadingProgress == null) {
+                                return child;
+                              } else {
+                                return const CircularProgressIndicator();
+                              }
+                            },
+                            errorBuilder: (context, error, stackTrace) {
+                              return const Text('Error loading image');
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${product['title']}',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '\$${product['price']}',
+                          style:
+                              TextStyle(color: Color.fromRGBO(40, 80, 180, 1)),
+                        ),
+                        Text(
+                          '${'${product['discountPercentage']}'.split(".").first}% · ${product['stock']} left',
+                          style: TextStyle(fontSize: 11),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
